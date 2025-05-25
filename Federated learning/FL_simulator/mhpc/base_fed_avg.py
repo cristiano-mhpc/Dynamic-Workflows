e9ac1548f17043bdbd23e236b8203239b5b9e3a7
christian_hpc <ticachristian20@gmail.com>
2025-05-25 21:18:51 +0800

    parallelized the trianing with dask

--- Federated learning/FL_simulator/mhpc/base_fed_avg.py
+++ Federated learning/FL_simulator/mhpc/base_fed_avg.py
@@ -12,6 +12,7 @@ import sys
 from typing import List
 import torch
 
+import dask
 from dask import delayed, compute
 from dask.distributed import Client
 
@@ -37,15 +38,13 @@ def test_client(client):
     print(f"Testing client {client.client_id}...")
     return client.test() 
 
-
 def training(args: argparse.Namespace) -> None:
     """FedAvg training script
 
     :param args: Command-line arguments
     :type args: argparse.Namespace
     """
-
-    # Launch local Dask cluster (optinal dashboard at localhost:8787)
+    # Launch local Dask cluster (dashboard at localhost:8787)
     dask_client = Client()
     print(f"Dask dashboard available at: {dask_client.dashboard_link}")
 
@@ -85,40 +84,48 @@ def training(args: argparse.Namespace) -> None:
 
     for i in range(args.rounds):
         print(f"Starting round {i}...")
+        if args.sequential:
+             # Sequential training
+            print("Training and testing the models sequentially from one client to the next...")
+            for id, client in enumerate(clients):
+                print(f"Client {id} training...")
+                # Client training
+                train_loss = client.train(args.epochs)
+                print(f"Client {id} finished training with loss: {train_loss:.4f}")
 
-        # Parallel training
-        train_tasks = [train_client(client, args.epochs) for client in clients]
-        train_results = compute(*train_tasks) # execute in parallel
+                print(f"Client {id} testing...")
+                # Client testing
+                test_loss = client.test()
+                print(f"Client {id} finished testing with loss: {test_loss[0]:.4f}, accuracy: {test_loss[1]:.2f}%")
+            
+            # Clients' models averaging
+            client_params = [client.get_parameters() for client in clients]
+        else: 
+            # Parallel training
+            print("Training and testing the models in parallel...")
+            train_tasks = [train_client(client, args.epochs) for client in clients]
+            train_results = compute(*train_tasks) # execute in parallel
 
+            # unpack training results
+            train_losses, client_params = zip(*train_results)
+            for i, loss in enumerate(train_losses):
+                print(f"Client {i} training loss: {loss:.4f}")
+            
+            # Parallel testing
+            test_tasks = [test_client(client) for client in clients]
+            test_results = compute(*test_tasks)
 
-        # for id, client in enumerate(clients):
-        #     print(f"Client {id} training...")
-        #     # Client training
-        #     train_loss = client.train(args.epochs)
-        #     print(f"Client {id} finished training with loss: {train_loss:.4f}")
+            for i, (loss, acc) in enumerate(test_results):
+                 print(f"Client {i} test loss: {loss:.4f}, accuracy: {acc:.2%}")
 
-        #     print(f"Client {id} testing...")
-        #     # Client testing
-        #     test_loss = client.test()
-        #     print(f"Client {id} finished testing with loss: {test_loss[0]:.4f}, accuracy: {test_loss[1]:.2f}%")
-        
-        # unpack training results
-        train_losses, client_params = zip(*train_results)
-        for i, loss in enumerate(train_losses):
-            print(f"Client {i} training loss: {loss:.4f}")
-        
-        # Parallel testing
-        test_tasks = [test_client(client) for client in clients]
-        test_results = compute(*test_tasks)
+            all_tasks = train_tasks + test_tasks
+            #Visualize the Dask task graph
+            dask.visualize(all_tasks, filename="train_test_graph", engine="graphviz", format="png")
 
-        for i, (loss, acc) in enumerate(test_results):
-             print(f"Client {i} test loss: {loss:.4f}, acc: {acc:.2%}")
+            print(f"Averaging clients' models...")
 
-        print(f"Averaging clients' models...")
-        # Clients' models averaging
-        #client_params = [client.get_parameters() for client in clients]
+        # Average clients' models
         server.average(client_params)
-
         # Sending aggregated model to clients
         for id, client in enumerate(clients):
             print(f"Sending model to client {id}...")
